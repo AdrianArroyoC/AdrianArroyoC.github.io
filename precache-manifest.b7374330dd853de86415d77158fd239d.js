b5bbae5809d255a5bb728ba77af123c89f06079c
AdrianArroyoC <adrianarroyoceja@gmail.com>
2020-07-20 19:41:17 -0500

    Updates

---
+++ precache-manifest.b7374330dd853de86415d77158fd239d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6f31b7469dbdd8e2ca285c2f800926",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b1d7ab6c80ae3574f47",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6f757ec8c35fcfeb667",
+    "url": "/static/js/2.04d5eebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04d5eebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1d7ab6c80ae3574f47",
+    "url": "/static/js/main.bddce508.chunk.js"
+  },
+  {
+    "revision": "446e250a2ed41d4625e0",
+    "url": "/static/js/runtime-main.ce922a46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
